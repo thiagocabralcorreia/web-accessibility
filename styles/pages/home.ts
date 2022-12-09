5fa27e8f4d12887c091f7bf8337cce515a775889
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2022-12-09 17:11:54 -0300

    First commit

---
+++ styles/pages/home.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  margin: 0 auto;
+  max-width: 750px;
+`;
+
+export const Title = styled.h1`
+  margin: 50px;
+  line-height: 1.15;
+  font-size: 4rem;
+  text-align: center;
+`;
+
+export const Description = styled.p`
+  margin-bottom: 40px;
+  line-height: 1.5;
+  font-size: 1.5rem;
+`;
